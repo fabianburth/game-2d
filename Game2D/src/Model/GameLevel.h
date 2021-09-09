dddc8c51c7633c13b0d4874eedea35164aa74c9d
RingTuEnigma <burthi1999@gmail.com>
2021-09-09 22:40:36 +0200

    Wrote some unit tests, removed some obsolete code and cleaned up the init method in GameLevel

--- Game2D/src/Model/GameLevel.h
+++ Game2D/src/Model/GameLevel.h
@@ -40,83 +40,60 @@ public:
     Wall TopWall;
     Wall BottomWall;
     std::vector<Block> Blocks;
+
+    /** Keeps track of the players score and provides useful methods */
     Score score;
-    //int score;
-    // Clock to track how long it takes the player to finish the level (relevant for score)
+
+    /** Clock to track how long it takes the player to finish the level (relevant for score) */
     std::clock_t startClockLevel;
-    // Clock to track how much time has passed since the last enemy was killed (relevant for enemy state)
+
+    /** Clock to track how much time has passed since the last enemy was killed (relevant for enemy state) */
     std::clock_t startClockEnemyKill;
-    //GameObject d;
-    //GameInformation P1;
-    //GameInformation Score;
+
     bool diamondBlocksAligned = false;
-    // constructor
+
     GameLevel();
     ~GameLevel();
 
     void registerObserver(Observer<GameLevel> *o) override;
     void removeObserver(Observer<GameLevel> *o) override;
     void notifyObservers() override;
-    // loads level from file
-    void Load(const char *file);
-    // render level
-    // void Draw(SpriteRenderer& renderer);
-    // check if the level is completed (all non-solid tiles are destroyed)
-    bool IsCompleted();
 
-    // Method which is called every game loop iteration which does some checks and updates the game state under certain conditions
-    // @param dt: The time passed since the last game loop iteration
-    void updateGameState(float dt);
+    /**
+     * Loads level from file
+     */
+    auto Load(const char *file) -> void;
+
+    /**
+     * Check if the level is completed (all non-solid tiles are destroyed)
+     */
+
+    auto IsCompleted() -> bool;
+
+    /**
+     * Method which is called every game loop iteration which does some checks and updates the game state under certain conditions
+     * @param dt: The time passed since the last game loop iteration
+     */
+    auto updateGameState(float dt) -> void;
 
 
     //! Following methods are moved from the Controller (Game) to this Model (GameLevel) to abide with MVC
-    // Checks whether the given gameObject is colliding with another block or a wall when moving one unit to the given direction
-    // @param gameObject: The GameObject to check the collision for
-    // @param direction: The Direction in which collision has to be checked
-    // @return true, if there is a collision, false otherwise
-    bool checkCollisions(GameObject& gameObject, Direction d);
+    /**
+     * Processes an attack of the Player (=Pressing LeftCtrl) and hence determines whether a block is pushed or destroyed or whether a wall will wobble
+     */
+    auto processPengoAttack() -> void;
 
-    // Return the Block with which the given GameObject is colliding
-    // @param gameObject: The GameObject to check the collision for
-    // @param direction: The Direction in which collision has to be checked
-    // @return a pointer to the gameObject with which the given GameObject is colliding or nullptr otherwise
-    Block* getCollisionBlock(GameObject& gameObject, Direction d);
+    /**
+     * Processes movement of the Player (=Pressing WASD) and hence determines where Pengo will go
+     */
+    auto processPengoMovement(Direction d) -> void;
 
-    // Calculates how many units the given GameObject could move until colliding with another block or the wall (step range)
-    // @param gameObject: The GameObject the step range has to be calculated for
-    // @param direction: The Direction in which the step range has to be checked
-    int  calculateStepRange(Block& block, Direction d);
-
-    // Checks whether the given gameObject is colliding with another gameObject
-    // @param one: The GameObject to check the collision
-    // @param two: The GameObject to check the collision
-    bool checkCollisionPrecise(GameObject& one, GameObject& two);
-
-    // Removes the enemy from the vector of active enemies
-    // @param enemy: A pointer to the enemy to be removed
-    void killEnemy(Enemy* enemy);
-
-    // Spawns one of the currently frozen enemies
-    // @return a pointer to the frozen enemy
-    Enemy* spawnEnemy();
-
-    // Checks whether an enemy of type boxer exists at the moment
-    // @return true, if one exists, false otherwise
-    bool boxerExists();
-
-    // Sets one of the remaining active enemies to type boxer if possible
-    void trySettingBoxer();
-
-    // Processes an attack of the Player (=Pressing LeftCtrl) and hence determines whether a block is pushed or destroyed or whether a wall will wobble
-    void processPengoAttack();
-
-    // Processes movement of the Player (=Pressing WASD) and hence determines where Pengo will go
-    void processPengoMovement(Direction d);
-
-    // Sets the states of blocks and enemies to an level start state 
-    // (Among other reasons, this is functionality is encapsulated in a seperate method and is not part of the init method, because this should be done AFTER
-    // the view is initialized so it gets informed about change of states)
-    void initStates();
+    /**
+     * Sets the states of blocks and enemies to an level start state
+     * (Among other reasons, this is functionality is encapsulated in a separate method and is not part of the init method, because this should be done AFTER
+     * the view is initialized so it gets informed about change of states)
+     */
+    auto initStates() -> void;
 
 protected:
     std::vector<Observer<GameLevel>*> observers;
@@ -129,62 +106,130 @@ protected:
 
     //! Following methods are moved from the Controller (Game) to this Model (GameLevel) to abide with MVC
 
-    /** Auxiliary method for checkCollisions - checks whether GameObject one collides with GameObject two if it is moved
-    * one unit in the given direction
-    * @param one: The GameObject to be moved
-    * @param two: The GameObject with which collision will be checked
-    * @param d: The Direction to which it would be moved
-    * @return true, if the blocks would collide, false otherwise
-    */
+    /**
+     * Auxiliary method for checkCollisions - checks whether GameObject one collides with GameObject two if it is moved
+     * one unit in the given direction
+     * @param one: The GameObject to be moved
+     * @param two: The GameObject with which collision will be checked
+     * @param d: The Direction to which it would be moved
+     * @return true, if the blocks would collide, false otherwise
+     */
     auto checkBlockCollision(GameObject& one, GameObject& two, Direction d) -> bool;
 
-    /** Auxiliary method for checkCollisions - checks whether GameObject will be colliding with the wall when moved one unit
-    * in the given Direction
-    * @param one: The GameObject to be moved
-    * @param d: The Direction to which it would be moved
-    * @return true, if it would collide with the wall, false otherwise
-    */
+    /**
+     * Auxiliary method for checkCollisions - checks whether GameObject will be colliding with the wall when moved one unit
+     * in the given Direction
+     * @param one: The GameObject to be moved
+     * @param d: The Direction to which it would be moved
+     * @return true, if it would collide with the wall, false otherwise
+     */
     auto checkWallCollision(GameObject& one, Direction d) -> bool;
 
-    /** Auxiliary method, checks whether the given gameObject is touching the wall at the given side (used to check every game loop iteration, whether enemies are touching the wall
-    * and need to be stunned, in case the wall is wobbly - thus and offset of 1 WIDTH_UNIT as in checkWallCollision would not make sense and lead to enemies being stunned too early)
-    * @param gameObject: The GameObject to check the collision for
-    * @param direction: The Direction in which collision has to be checked
-    * @return true, if it is touching the wall, false otherwise
-    */
+    /**
+     * Auxiliary method, checks whether the given gameObject is touching the wall at the given side (used to check every game loop iteration, whether enemies are touching the wall
+     * and need to be stunned, in case the wall is wobbly - thus and offset of 1 WIDTH_UNIT as in checkWallCollision would not make sense and lead to enemies being stunned too early)
+     * @param gameObject: The GameObject to check the collision for
+     * @param direction: The Direction in which collision has to be checked
+     * @return true, if it is touching the wall, false otherwise
+     */
     auto checkWallCollisionPrecise(GameObject& one, Direction d) -> bool;
 
-    // Auxiliary method which returns a vector containing all directions which are possible for the enemy to move to
-    // 1) In case the enemy is in state wandering, the vector is ordered like:
-    //    [current direction, not current and not opposite, not current and not opposite, opposite direction]
-    //    and then the not possible directions (because there is a wall or a block) are removed from the array
-    // 2) In case the enemy is in state chasing, the vector is ordered like:
-    //    [direction with the greatest distance to pengo, not greatest distance to pengo and not opposite, not greatest distance to pengo and not opposite, opposite direction]
-    //    and then the not possible directions (because there is a wall or an unbreakable block) are removed from the array
-    // @param enemy: The enemy for which the directions array shall get generated
-    // @return: The vector containing the possible directions
+    /**
+     * Auxiliary method which returns a vector containing all directions which are possible for the enemy to move to
+     * 1) In case the enemy is in state wandering, the vector is ordered like:
+     *    [current direction, not current and not opposite, not current and not opposite, opposite direction]
+     *    and then the not possible directions (because there is a wall or a block) are removed from the array
+     * 2) In case the enemy is in state chasing, the vector is ordered like:
+     *    [direction with the greatest distance to pengo, not greatest distance to pengo and not opposite, not greatest distance to pengo and not opposite, opposite direction]
+     *    and then the not possible directions (because there is a wall or an unbreakable block) are removed from the array
+     * @param enemy: The enemy for which the directions array shall get generated
+     * @return: The vector containing the possible directions
+     */
     auto getInitialDirections(Enemy& enemy) -> std::vector<Direction>;
 
-    // Auxiliary method which determines (an index of a) direction based on the probability vector
-    // @param chances: A probability vector for this enemy
-    // @return: The index of the direction in a direction vector
+    /**
+     * Auxiliary method which determines (an index of a) direction based on the probability vector
+     * @param chances: A probability vector for this enemy
+     * @return: The index of the direction in a direction vector
+     */
     auto getDirectionIndex(std::vector<int> chances) -> int;
 
-    // Auxiliary method which checks whether 3 diamond blocks got aligned and updates the score if so
+    /**
+     * Auxiliary method which checks whether 3 diamond blocks got aligned and updates the score if so
+     */
     void checkThreeDiamonds();
 
-    // Auxiliary method which checks whether a block touches a wall
-    // @param b: The block to check whether it touches the wall
-    // @return true, if it touches the wall, false otherwise
+    /**
+     * Auxiliary method which checks whether a block touches a wall
+     * @param b: The block to check whether it touches the wall
+     * @return true, if it touches the wall, false otherwise
+     */
     auto blockTouchesWall(Block& b) -> bool;
 
-    // Auxiliary method which determines and update the game state concerning bots behavior, movement and their interaction with the player and the environment
-    // @param dt: The time passed since the last game loop iteration
+    /**
+     * Auxiliary method which determines and update the game state concerning bots behavior, movement and their interaction with the player and the environment
+     * @param dt: The time passed since the last game loop iteration
+     */
     auto determineBotBehavior(float dt) -> void;
 
-    // Auxiliary method which updates the game state concerning block movement and their interaction with the environment
-    // @param dt: The time passed since the last game loop iteration
+    /**
+     * Auxiliary method which updates the game state concerning block movement and their interaction with the environment
+     * @param dt: The time passed since the last game loop iteration
+     */
     auto updateBlockInteractions(float dt) -> void;
+
+    /**
+     * Sets one of the remaining active enemies to type boxer if possible
+     */
+    auto trySettingBoxer() -> void;
+
+    /**
+     * Checks whether an enemy of type boxer exists at the moment
+     * @return true, if one exists, false otherwise
+     */
+    auto boxerExists() -> bool;
+
+    /**
+     * Spawns one of the currently frozen enemies
+     * @return a pointer to the frozen enemy
+     */
+    auto spawnEnemy() -> Enemy*;
+
+    /**
+     * Checks whether the given gameObject is colliding with another block or a wall when moving one unit to the given direction
+     * @param gameObject: The GameObject to check the collision for
+     * @param direction: The Direction in which collision has to be checked
+     * @return true, if there is a collision, false otherwise
+     */
+    auto checkCollisions(GameObject& gameObject, Direction d) -> bool;
+
+    /**
+     * Return the Block with which the given GameObject is colliding
+     * @param gameObject: The GameObject to check the collision for
+     * @param direction: The Direction in which collision has to be checked
+     * @return a pointer to the gameObject with which the given GameObject is colliding or nullptr otherwise
+     */
+    auto getCollisionBlock(GameObject& gameObject, Direction d) -> Block*;
+
+    /**
+     * Calculates how many units the given GameObject could move until colliding with another block or the wall (step range)
+     * @param gameObject: The GameObject the step range has to be calculated for
+     * @param direction: The Direction in which the step range has to be checked
+     */
+    auto calculateStepRange(Block& block, Direction d) -> int;
+
+    /**
+     * Checks whether the given gameObject is colliding with another gameObject
+     * @param two: The GameObject to check the collision
+     * @param one: The GameObject to check the collision
+     */
+    auto checkCollisionPrecise(GameObject& one, GameObject& two) -> bool;
+
+    /**
+     * Removes the enemy from the vector of active enemies
+     * @param enemy: A pointer to the enemy to be removed
+     */
+    auto killEnemy(Enemy* enemy) -> void;
 };
 
 #endif
--- Tests/TestGameLevel.cpp
+++ Tests/TestGameLevel.cpp
@@ -6,7 +6,7 @@
 
 #include "gtest/gtest.h"
 
-struct TestGameLevel: public GameLevel {
+struct TestGameLevel : public GameLevel {
     using GameLevel::blockTouchesWall;
 
     using GameLevel::checkWallCollision;
@@ -16,15 +16,17 @@ struct TestGameLevel: public GameLevel {
 
     using GameLevel::checkCollisions;
     using GameLevel::checkCollisionPrecise;
+
+    using GameLevel::calculateStepRange;
 };
 
-TEST(GameLevel, BlockTouchesWall){
+TEST(GameLevel, BlockTouchesWall) {
     TestGameLevel testGameLevel = TestGameLevel();
 
     Block testBlock0 = Block({0, 0}, false, nullptr, BlockState::SOLID);
-    Block testBlock1 = Block({Constants::WIDTH_UNIT, Constants::HEIGHT_UNIT}, false, nullptr, BlockState::SOLID) ;
-    Block testBlock2 = Block({Constants::WIDTH_UNIT*12, Constants::HEIGHT_UNIT*14}, false, nullptr, BlockState::SOLID);
-    Block testBlock3 = Block({Constants::WIDTH_UNIT, Constants::HEIGHT_UNIT*12}, false, nullptr, BlockState::SOLID);
+    Block testBlock1 = Block({Constants::WIDTH_UNIT, Constants::HEIGHT_UNIT}, false, nullptr, BlockState::SOLID);
+    Block testBlock2 = Block({Constants::WIDTH_UNIT * 12, Constants::HEIGHT_UNIT * 14}, false, nullptr, BlockState::SOLID);
+    Block testBlock3 = Block({Constants::WIDTH_UNIT, Constants::HEIGHT_UNIT * 12}, false, nullptr, BlockState::SOLID);
 
 
     EXPECT_EQ(true, testGameLevel.blockTouchesWall(testBlock0));
@@ -33,15 +35,15 @@ TEST(GameLevel, BlockTouchesWall){
     EXPECT_EQ(false, testGameLevel.blockTouchesWall(testBlock3));
 }
 
-TEST(GameLevel, CheckWallCollision){
+TEST(GameLevel, CheckWallCollision) {
     TestGameLevel testGameLevel = TestGameLevel();
 
     GameObject testObject0 = GameObject({0, 0});
-    GameObject testObject1 = GameObject({Constants::WIDTH_UNIT, Constants::HEIGHT_UNIT}) ;
-    GameObject testObject2 = GameObject({Constants::WIDTH_UNIT*12, Constants::HEIGHT_UNIT*14});
-    GameObject testObject3 = GameObject({Constants::WIDTH_UNIT, Constants::HEIGHT_UNIT*12});
+    GameObject testObject1 = GameObject({Constants::WIDTH_UNIT, Constants::HEIGHT_UNIT});
+    GameObject testObject2 = GameObject({Constants::WIDTH_UNIT * 12, Constants::HEIGHT_UNIT * 14});
+    GameObject testObject3 = GameObject({Constants::WIDTH_UNIT, Constants::HEIGHT_UNIT * 12});
     GameObject testObject4 = GameObject({Constants::WIDTH_UNIT * 11, 0});
-    GameObject testObject5 = GameObject({Constants::WIDTH_UNIT * 11.5f , 0 });
+    GameObject testObject5 = GameObject({Constants::WIDTH_UNIT * 11.5f, 0});
 
     EXPECT_EQ(false, testGameLevel.checkWallCollision(testObject0, Direction::RIGHT));
     EXPECT_EQ(false, testGameLevel.checkWallCollision(testObject0, Direction::UP));
@@ -68,11 +70,11 @@ TEST(GameLevel, CheckWallCollision){
     EXPECT_EQ(false, testGameLevel.checkWallCollisionPrecise(testObject5, Direction::RIGHT));
 }
 
-TEST(GameLevel, CheckBlockCollision){
+TEST(GameLevel, CheckBlockCollision) {
     TestGameLevel testGameLevel = TestGameLevel();
 
     // This object is in the bottom left corner
-    GameObject testObject0 = GameObject({0,0});
+    GameObject testObject0 = GameObject({0, 0});
     // This object is one unit to the right of testObject0 and at the same height
     GameObject testObject1 = GameObject({Constants::WIDTH_UNIT, 0});
     // This object is two units to the right of testObject0 and at the same height
@@ -86,7 +88,7 @@ TEST(GameLevel, CheckBlockCollision){
 
 
     // This object is in the bottom left corner
-    GameObject testObject3 = GameObject({0,0});
+    GameObject testObject3 = GameObject({0, 0});
     // This object is one unit above testObject3
     GameObject testObject4 = GameObject({0, Constants::HEIGHT_UNIT});
     // This object is two units above testObject3
@@ -98,3 +100,55 @@ TEST(GameLevel, CheckBlockCollision){
     EXPECT_EQ(true, testGameLevel.checkBlockCollision(testObject4, testObject3, Direction::DOWN));
     EXPECT_EQ(false, testGameLevel.checkBlockCollision(testObject5, testObject3, Direction::DOWN));
 }
+
+TEST(GameLevel, CheckCollision) {
+    TestGameLevel testGameLevel = TestGameLevel();
+
+    GameObject testObject0 = GameObject({Constants::WIDTH_UNIT * 2, Constants::HEIGHT_UNIT * 2});
+    GameObject testObject1 = GameObject({0, 0});
+
+    Block testBlock0 = Block({Constants::WIDTH_UNIT * 2, Constants::HEIGHT_UNIT * 3}, false, nullptr, BlockState::SOLID);
+    Block testBlock1 = Block({Constants::WIDTH_UNIT * 3, Constants::HEIGHT_UNIT * 2}, false, nullptr, BlockState::SOLID);
+    Block testBlock2 = Block({0, Constants::HEIGHT_UNIT * 2}, false, nullptr, BlockState::SOLID);
+    Block testBlock3 = Block({Constants::WIDTH_UNIT * 2, 0}, false, nullptr, BlockState::SOLID);
+    Block testBlock4 = Block({Constants::WIDTH_UNIT * 1.5f, Constants::HEIGHT_UNIT * 2}, false, nullptr, BlockState::SOLID);
+    Block testBlock5 = Block({Constants::WIDTH_UNIT * 0.5f, Constants::HEIGHT_UNIT * 2}, false, nullptr, BlockState::SOLID);
+
+    testGameLevel.Blocks.push_back(testBlock0);
+    testGameLevel.Blocks.push_back(testBlock1);
+    testGameLevel.Blocks.push_back(testBlock2);
+    testGameLevel.Blocks.push_back(testBlock3);
+
+    EXPECT_EQ(true, testGameLevel.checkCollisions(testObject0, Direction::RIGHT));
+    EXPECT_EQ(true, testGameLevel.checkCollisions(testObject0, Direction::UP));
+    EXPECT_EQ(false, testGameLevel.checkCollisions(testObject0, Direction::DOWN));
+    EXPECT_EQ(false, testGameLevel.checkCollisions(testObject0, Direction::LEFT));
+    EXPECT_EQ(true, testGameLevel.checkCollisions(testObject1, Direction::LEFT));
+
+    EXPECT_EQ(false, testGameLevel.checkCollisionPrecise(testObject0, testBlock0));
+    EXPECT_EQ(false, testGameLevel.checkCollisionPrecise(testObject0, testBlock5));
+    EXPECT_EQ(true, testGameLevel.checkCollisionPrecise(testObject0, testBlock4));
+}
+
+TEST(GameLevel, StepRange) {
+    TestGameLevel testGameLevel = TestGameLevel();
+
+    Block testBlock0 = Block({Constants::WIDTH_UNIT * 2, Constants::HEIGHT_UNIT * 2}, false, nullptr, BlockState::SOLID);
+
+
+    Block testBlock1 = Block({Constants::WIDTH_UNIT * 3, Constants::HEIGHT_UNIT * 2}, false, nullptr, BlockState::SOLID);
+    Block testBlock2 = Block({Constants::WIDTH_UNIT * 2, Constants::HEIGHT_UNIT * 10}, false, nullptr, BlockState::SOLID);
+    Block testBlock3 = Block({Constants::WIDTH_UNIT * 2, 0}, false, nullptr, BlockState::SOLID);
+
+    testGameLevel.Blocks.push_back(testBlock0);
+    testGameLevel.Blocks.push_back(testBlock1);
+    testGameLevel.Blocks.push_back(testBlock2);
+    testGameLevel.Blocks.push_back(testBlock3);
+
+    EXPECT_EQ(2, testGameLevel.calculateStepRange(testBlock0, Direction::LEFT));
+    EXPECT_EQ(0, testGameLevel.calculateStepRange(testBlock0, Direction::RIGHT));
+    EXPECT_EQ(7, testGameLevel.calculateStepRange(testBlock0, Direction::UP));
+    EXPECT_EQ(1, testGameLevel.calculateStepRange(testBlock0, Direction::DOWN));
+}
+
+
